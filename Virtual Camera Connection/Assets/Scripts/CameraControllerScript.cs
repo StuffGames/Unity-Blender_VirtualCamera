788eee62ea403ff34626ba9c817622033e9593a6
StuffGames <44318828+StuffGames@users.noreply.github.com>
2019-12-19 12:44:11 -0500

    Second Commit
    Just introducing the script and some functionality with my iPhone. You can look around with a phone in the Scene. Next up would be transfering data to Blender or a format that can be thrown in Blender.

---
+++ Virtual Camera Connection/Assets/Scripts/CameraControllerScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.iOS;
+
+public class CameraControllerScript : MonoBehaviour
+{
+    private GameObject gObject;
+    //public Camera mainCam;
+    Gyroscope gyro;
+
+    public float xFloat, yFloat, zFloat, wFloat;
+
+    void Start()
+    {
+        gyro = Input.gyro;
+        gyro.enabled = true;
+        gObject = new GameObject("Camera Container");
+        gObject.transform.position = transform.position;
+        transform.SetParent(gObject.transform);
+        gObject.transform.rotation = Quaternion.Euler(90f,90f,0);
+    }
+
+    void Update()
+    {
+        GyroModifyCamera();
+        Debug.Log(gyro.gravity);
+        transform.localRotation = gyro.attitude * new Quaternion(0,0,1,0);
+    }
+
+    void GyroModifyCamera()
+    {
+        transform.rotation = GyroToUnity(Input.gyro.attitude);
+    }
+
+    private static Quaternion GyroToUnity(Quaternion q)
+    {
+        return new Quaternion(q.x, q.y, q.z, q.w);
+    }
+}
