d316a7ebf0d613e598d423b10c192d1cb8dc337e
StuffGames <44318828+StuffGames@users.noreply.github.com>
2019-12-29 20:35:12 -0500

    Fourth Commit
    This is the one that fully integrates data sending to Blender through a text file, lol. Parsing by JSON will probably come later through the project progression. Um, for the most part it's just getting the values correctly and formatting them correctly so it is easy to read and to send through a text file so that the values can them be plucked out fairly easily and put into the properties in Blender. Although there is still much needed work to create the finished product as I was just testing whether values can be sent through Unity to Blender and yes! The extraction of the values though probably requires more thourough reformatting of the values until it becomes efficient. This is practically just showing that yes we can create and set values in a timeline for animation and send them through a text file to Blender that can be read INSIDE Blender. I probably will just need to tweak a few more things in code to finish the C# and Unity side of things before comepletely starting the development of the Blender add-on. These are just purely to show that the movements and keyframes can be transported, and that the movement can be replicated; although I do see a problem with having the proper rotation of things, that will be an issue to fix in the future once I make sure that everything is working. Quaternion values have been truncated to the tenths place which I could have a problem with so I will probably fix that later once I make sure things work properly.

--- Virtual Camera Connection/Assets/Scripts/AnimationControlScript.cs
+++ Virtual Camera Connection/Assets/Scripts/AnimationControlScript.cs
@@ -1,55 +1,130 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEditor.Animations;
+using UnityEditor;
 using UnityEngine.UI;
 
 public class AnimationControlScript : MonoBehaviour
 {
-    private GameObjectRecorder objRec;
-    public Animator animControl;
-    public AnimationClip anim;
-    public Text placementText;
+    private GameObjectRecorder objRec; //ObjectRecorder to do the auto keyframing
+    public Animator animControl; //Animation controller of separate animation
+    public AnimationClip anim; //The animation file to record on
+    public Text placementText; //Text used to display time
+    public AnimationCurve animCurve;
 
-    private bool timeToPlay = false;
+    public int frameStop; //Frame to stop the recording
+    private bool timeToPlay = false; //Used to control when to start and stop recording
+    private string path = @"C:\Program Files (x86)\Steam\steamapps\common\Blender\QuaternionKeyFrameValues.txt"; //Directory to text file
+    public List<Quaternion> keyFrames = new List<Quaternion>(); //Holds overall Quaternion rotation values for each frame
+    public List<AnimationCurve> curves = new List<AnimationCurve>(); //Holds the animation curves for each value of the Quaternion rotation values
+    public List<Vector2> xKeys = new List<Vector2>(); //Holds the frame and value of the x value of the Quaternion rotation
+    public List<Vector2> yKeys = new List<Vector2>(); //" " y value of Quaternion
+    public List<Vector2> zKeys = new List<Vector2>(); //" " z value of Quaternion
+    public List<Vector2> wKeys = new List<Vector2>(); //" " w value of Quaternion
 
+    //Initiates some references and methods at the start of being played
     void Start()
     {
-        //anim.frameRate = 24;
         objRec = new GameObjectRecorder(gameObject);
         objRec.BindComponentsOfType<Transform>(gameObject, false);
+        print(transform.rotation);
+        print(transform.localRotation);
     }
-    bool startedRecording = false;
-    bool savedRecording = false;
+
+    bool startedRecording = false; //To check if the recording started
+    bool savedRecording = false; //To check if the recording saved
+    
+    //LateUpdate used to run code at the end of a frame update so less desync occurs
     void LateUpdate()
     {
-        print(objRec.isRecording);
-        placementText.text = objRec.currentTime.ToString();
+        int currentFrame = Mathf.FloorToInt(objRec.currentTime * 24.0f);
+        //print(objRec.isRecording);
+        placementText.text = "Frame: " + Mathf.Floor(objRec.currentTime * 24.0f).ToString();
         if (timeToPlay)
         {
             objRec.TakeSnapshot(Time.deltaTime);
             startedRecording = true;
         }
+
         else if(!timeToPlay && startedRecording)
         {
             if (!savedRecording)
             {
+                EditorCurveBinding[] someList;
+                someList = objRec.GetBindings();
                 objRec.SaveToClip(anim, 24.0f);
                 objRec.ResetRecording();
                 savedRecording = true;
+                print(transform.rotation);
+                print(transform.localRotation);
+                for (int i = 0; i < someList.Length; i++)
+                {
+                    curves.Add(AnimationUtility.GetEditorCurve(anim, someList[i]));
+                }
+
+                for (int i = 0; i < 4; i++)
+                {
+                    for(int k = 0; k < curves[i].keys.Length; k++)
+                    {
+                        float t = curves[i].keys[k].time;
+                        float v = curves[i].keys[k].value;
+                        if (i == 0)
+                        {
+                            xKeys.Add(new Vector2(t, v));
+                        }
+                        else if(i == 1)
+                        {
+                            yKeys.Add(new Vector2(t, v));
+                        }
+                        else if(i == 2)
+                        {
+                            zKeys.Add(new Vector2(t, v));
+                        }
+                        else if (i == 3)
+                        {
+                            wKeys.Add(new Vector2(t, v));
+                        }
+                    }
+                }
+                //Write on Text File Stuff
+                if (File.Exists(path))
+                {
+                    for (int i = 0; i < xKeys.Count; i++)
+                    {
+                        string frameInfo;
+                        Quaternion frameQuaternion = new Quaternion(xKeys[i].y, yKeys[i].y, zKeys[i].y, wKeys[i].y);
+                        keyFrames.Add(frameQuaternion);
+                        frameInfo = Mathf.FloorToInt(xKeys[i].x * 24.0f) + "," + wKeys[i].y + "," + xKeys[i].y + "," + yKeys[i].y + "," + zKeys[i].y + "\n";
+                        //print(frameInfo);
+                        File.AppendAllText(path, frameInfo);
+                    }
+                    Debug.Log("Done");
+                }
             }
             else
             {
                 return;
             }
         }
+
+        if (frameStop > 0)
+        {
+            if (frameStop == Mathf.Floor(objRec.currentTime * 24.0f))
+            {
+                timeToPlay = false;
+            }
+        }
     }
 
+    //Empty Method to be considered or use later
     public void ResetAnimation()
     {
         //objRec.ResetRecording();
     }
 
+    //Public Method used to get input from the Record UI button
     public void Record()
     {
         if (timeToPlay)
@@ -63,8 +138,9 @@ public class AnimationControlScript : MonoBehaviour
         }
     }
 
-    bool thing = false;
+    bool thing = false; //Just used as a switch from on and off for below method
 
+    //Public Method used by the UI Play button to play and stop the separate animation
     public void PlayAnimation()
     {
 
